b9247a9c8d2e3e71b841f238791faa10d858e628
Ronan LEGARDINIER <ronan.legardinier@gmail.com>
2016-01-14 18:33:41 +0100

    Modification du main

--- src/main/java/fr/iut/nantes/tcpserver/HighLevelServer.java
+++ src/main/java/fr/iut/nantes/tcpserver/HighLevelServer.java
@@ -1,7 +1,11 @@
 package fr.iut.nantes.tcpserver;
 
+/**
+ * Serveur - Haut niveau
+ * @author Ronan / Ugho
+ */
 public class HighLevelServer {
-	public static void main(String args[]) throws Exception {
+	public static void main(String args[]) {
 		
 	}
 }
